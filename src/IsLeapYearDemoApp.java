7c2769562d5d7f9cb4b59d601a931d654bea329e
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-15 10:42:13 +0100

    Schaltjahr

--- src/IsLeapYearDemoApp.java
+++ src/IsLeapYearDemoApp.java
@@ -2,12 +2,53 @@ public class IsLeapYearDemoApp {
 
     public static void main(String[] args) {
 
+
+        int[] arr = {87, 12, 5, 3, 4, 1, 45};
+
+        int count = 0;
+        for (int i = 0; i <= 2100; i++) {
+            if (isLeapYear(i, true))
+            {
+                count++;
+            }
+        }
+        System.out.println("count = " + count);
     }
 
+    public static boolean isLeapYear(int year) {
+        return isLeapYear(year, false);
+    }
 
-    public static void isLeapYear(int year){
+    /*
+       4 => Schaltjahr
+      16 => Schaltjahr
+    1992 => Schaltjahr (da durch 4 teilbar)
+    1996 => Schaltjahr (da durch 4 teilbar)
+    2020 => Schaltjahr (da durch 4 teilbar)
 
+    1900 => kein Schaltjahr (ist zwar durch 4 teilbar,
+                gleichzeitig aber auch durch 100, nicht jedoch durch 400)
+    2000 => Schaltjahr (da durch 4 und 400 teilbar)
+    2100 => kein Schaltjahr
+     */
+    public static boolean isLeapYear(int year, boolean showPrintMessages){
 
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                if (showPrintMessages)
+                    System.out.println(year + " is a leap year");
 
+                return true;
+            }
+        }
+        else {
+            if (year % 4 == 0){
+                if (showPrintMessages)
+                    System.out.println(year + " is a leap year");
+
+                return true;
+            }
+        }
+        return false;
     }
 }
