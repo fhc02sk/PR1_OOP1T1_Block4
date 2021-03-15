613302935b5b35e01b9a860db1e0d93d6b5eecb9
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-15 12:03:31 +0100

    ArrayDemoApp

--- src/ArrayDemoApp.java
+++ src/ArrayDemoApp.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class ArrayDemoApp {
     public static void main(String[] args) {
 
-        int[] arr = {87, 12, 5, 3, 4, 1, 45, 17, 31, 3};
+        int[] arr = {87, 12, 5, 3, 4, 1, 45, 17, 31, 3, 5, 7};
 
         printArray(arr);
 
@@ -11,8 +13,26 @@ public class ArrayDemoApp {
 
         System.out.println("calculateSum(arr) = " + calculateSum(arr));
 
-        int[] reversedarr = reverse(arr);
-        printArray(reversedarr);
+        System.out.println("Hello World");
+
+        int[] ihrName = reverse(arr);
+        System.out.println("StringResult: " + arrayToString(ihrName));
+
+        int[] reversedarr2 = reverse2(arr);
+        printArray(reversedarr2);
+        System.out.print("Arr: ");
+        printArray(arr);
+
+        int[] reversearr3 = reverse3(arr);
+        System.out.println("Variante 3: " + Arrays.toString(reversearr3));
+    }
+
+    public static String arrayToString(int[] numbers) {
+        String result = "";
+        for (int i = 0; i < numbers.length; i++) {
+            result = result + numbers[i] + ", ";
+        }
+        return result;
     }
 
     public static void printArray(int[] numbers) {
@@ -43,19 +63,43 @@ public class ArrayDemoApp {
         /*
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
+        }
          */
         
         return sum;
     }
 
     public static int[] reverse(int[] numbers){
-        int[] result = new int[numbers.length];
+        int[] reversedResult = new int[numbers.length];
 
         for (int i = 0; i < numbers.length; i++) {
+            int newIndex = numbers.length -1 -i;
+            reversedResult[newIndex] = numbers[i];
+        }
+        return reversedResult;
+    }
+
+    public static int[] reverse2(int[] numbers){
+        for (int i = 0; i < numbers.length / 2; i++){
             int newIndex = numbers.length -1 - i;
-            result[newIndex] = numbers[i];
+            int temp = numbers[i]; // 87
+            numbers[i] = numbers[newIndex];
+            numbers[newIndex] = temp;
+        }
+        return numbers;
+    }
+
+    public static int[] reverse3(int[] numbers) {
+
+        int[] reversedResult = new int[numbers.length];
+        int newIndex = numbers.length - 1; // 9
+
+        for(int i = 0; i < numbers.length; i++){
+            reversedResult[newIndex] = numbers[i];
+            newIndex--;
         }
 
-        return result;
+        return reversedResult;
     }
+
 }
